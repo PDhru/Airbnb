c089658ec40fd55a52c7256a379b5623b5e24893
[PDhru] <[dhrutip2005@gmail.com]>
2024-11-22 20:42:20 +0530

    first commit

---
+++ app/components/Avtar.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Image from "next/image"
+
+const Avtar = () => {
+  return (
+    <Image className="rounded-full" height = "30" width = "30"
+    alt="Avtar"
+    src="/images/placeholder.png"/>
+  )
+}
+
+export default Avtar
---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+
+
+const font = Nunito({
+  subsets: ["latin"],
+})
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <Modal actionLabel="Submit" title="Hello world" isOpen />
+          <Navbar />
+        </ClientOnly>
+        {children}</body>
+    </html>
+  );
+}
