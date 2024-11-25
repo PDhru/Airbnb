8638237e7c4a35ce9fc50d042b8dfcb0300afa20
[PDhru] <[dhrutip2005@gmail.com]>
2024-11-25 21:25:23 +0530

    github

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import RegistarModal from "./components/modals/RegistarModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
 
 
 const font = Nunito({
@@ -27,6 +28,7 @@ export default function RootLayout({
         <ClientOnly>
           <ToasterProvider />
           <RegistarModal />
+          <LoginModal />
           <Navbar />
         </ClientOnly>
         {children}</body>
